c6417379dc206fec99d2ee2a5f64b363a7ac948b
Juha Myllyaho <juha.myllyaho@centria.fi>
2025-11-08 17:38:45 +0200

    added code

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -24,8 +24,64 @@ def main():
     with open(varaukset, "r", encoding="utf-8") as f:
         varaus = f.read().strip()
 
+        #tietojen jako
+        varausId = varaus.split('|')
+
+        #päivämäärän tuonti
+        from datetime import datetime
+
+        #tietotyyppien määritys
+        varausId[0] = int(varausId[0])
+        varausId[1] = str(varausId[1])
+        #muunnetaan päivämäärät ja kellonajat
+        varausId[2] = datetime.strptime(varausId[2],"%Y-%m-%d").date()
+        varausId[3] = datetime.strptime(varausId[3],"%H:%M").time()
+        varausId[4] = int(varausId[4])
+        varausId[5] = float(varausId[5])
+        #lasketaan kokonaishinta
+        varausId[6] = varausId[4]*varausId[5]
+        #lisätään listaan
+        varausId.insert(6, varausId[6])
+        #IF/ELSE lauseke
+        varausId[7] = "KYLLÄ" if varausId[7] else "EI"
+        varausId[8] = str(varausId[8])
+        varausId[9] = str(varausId[9])
+        varausId[10] = str(varausId[10])
+
+        #tehdään kopio ID listasta ja lisätään euron merkit tulosteeseen
+        tuloste = varausId.copy()
+
+        tuloste[5] = f"{tuloste[5]} €"
+        tuloste[6] = f"{tuloste[6]} €"
+
+        #otsikot
+        otsikot = [
+          "Varausnumero",
+          "Varaaja",
+          "Päivämäärä",
+          "Aloitusaika",
+          "Tuntimäärä",
+          "Tuntihinta",
+          "Kokonaishinta",
+          "Maksettu",
+          "Kohde",
+          "Puhelin",
+          "Sähköposti"  
+        ]
+
+
+
+        for otsikot, tuloste in zip(otsikot, tuloste):
+            print(f"{otsikot}: {tuloste}")
+
+       
+
+
+
+
+
     # Tulostetaan varaus konsoliin
-    print(varaus)
+    #print(varaus.split('|'))
 
     # Kokeile näitä
     #print(varaus.split('|'))
@@ -34,6 +90,7 @@ def main():
     #print(type(varausId))
     """
     Edellisen olisi pitänyt tulostaa numeron 123, joka
+
     on oletuksena tekstiä.
 
     Voit kokeilla myös vaihtaa kohdan [0] esim. seuraavaksi [1]
