a29e457bed4066c146cb0662c9c741d298a407a1
Juha Myllyaho <juha.myllyaho@centria.fi>
2025-11-18 18:27:52 +0200

    A part

--- Viikko4/lue_varaukset.py
+++ Viikko4/lue_varaukset.py
@@ -29,16 +29,16 @@ def muunna_varaustiedot(varaus: list) -> list:
     # Ensimmäisen alkion = varaus[0] muunnos
     muutettuvaraus.append(int(varaus[0]))
     # Ja tästä jatkuu
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
-    muutettuvaraus.append("")
+    muutettuvaraus.append(str(varaus[1]))
+    muutettuvaraus.append(str(varaus[2]))
+    muutettuvaraus.append(str(varaus[3]))
+    muutettuvaraus.append(datetime.strptime(varaus[4].strip(),"%Y-%m-%d").date())
+    muutettuvaraus.append(datetime.strptime(varaus[5].strip(),"%H:%M").time())
+    muutettuvaraus.append(int(varaus[6]))
+    muutettuvaraus.append(float(varaus[7]))
+    muutettuvaraus.append(bool(varaus[8]))
+    muutettuvaraus.append(str(varaus[9]))
+    muutettuvaraus.append(datetime.strptime(varaus[10].strip(),"%Y-%m-%d %H:%M:%S"))
     return muutettuvaraus
 
 def hae_varaukset(varaustiedosto: str) -> list:
